4912de6572423f64c288f75dd69369e119896283
Fitz Hawke <will@fitzhawk.com>
2022-04-08 03:04:28 -0400

    Completed nasa api homework

--- week-13/api-projects/api-template/js/main.js
+++ week-13/api-projects/api-template/js/main.js
@@ -3,7 +3,8 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  //const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const url = "https://www.codewars.com/api/v1/users/FitzHawke"
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
--- week-13/objects-nasa/js/main.js
+++ week-13/objects-nasa/js/main.js
@@ -1,1 +1,21 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+let url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+document.querySelector("button").addEventListener("click", getImage);
+
+function getImage() {
+    let date = document.querySelector("input").value;
+    console.log(date);
+    fetch(url + "&date=" + date)
+        .then((res) => res.json()) // parse response as JSON
+        .then((data) => {
+            console.log(data);
+            document.querySelector(
+                "h2"
+            ).innerText = `${data.date} ${data.title} - ${data.copyright}`;
+            document.querySelector("img").src = data.hdurl;
+            document.querySelector("h3").innerText = data.explanation;
+        })
+        .catch((err) => {
+            console.log(`error ${err}`);
+        });
+}
